7b2840529ceae3737c0f59bc471a4f6eba93d24b
zx4545zx <zx4545zx>
2022-05-06 17:45:38 +0700

    navbar is don't show to admin login

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,16 @@
 import { useRouter } from "next/router";
-
 import Layout from "./compoment/Layout/Layout";
 import AdminLayout from "./compoment/Layout/AdminLayout";
 import findRouterAdmin from "./lib/admin/findRouterAdmin";
-
 import "../styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  if (findRouterAdmin(router.pathname, "admin")) {
+  if (router.pathname === "/admin") {
+    return <Component {...pageProps} />;
+  } else if (findRouterAdmin(router.pathname, "/admin/")) {
     return (
       <AdminLayout>
         <Component {...pageProps} />
