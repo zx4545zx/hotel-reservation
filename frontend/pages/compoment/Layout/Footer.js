819bed91353b9322804126e68c4045247ebd25f5
Nattapon Hoedkhunthod <70090705+zx4545zx@users.noreply.github.com>
2022-05-28 12:16:15 +0700

    Ben (#18)
    * add customer button
    
    * Link to the customer list menu page
    
    * Additional booking status page and link to the quotation menu page
    
    * Design a payment confirmation page
    
    * aa
    
    * Add details to the home page
    
    * update
    
    * Update
    
    * Update
    
    * update
    
    * bb
    
    Co-authored-by: [Benjawan1999] <86357740+Benjawan1999@users.noreply.github.com>
    Co-authored-by: zx4545zx <zx4545zx>

--- frontend/pages/compoment/Layout/Footer.js
+++ frontend/pages/compoment/Layout/Footer.js
@@ -1,15 +1,34 @@
+import Image from "next/image";
+import MB2 from "../../public/MB2.jpg";
+
 const Footer = () => {
   return (
     <footer className="footer p-3">
       <div className="content has-text-centered">
-        <h1 className="is-size-4">CONTACTS US</h1>
+        <h1 className="is-size-4">FACILITIES AND SERVICES</h1>
         <p>
-          204/2 Soi Arun Amarin49, Arun Amarin Rd., Bangyikhan, Bangplud,
-          Bangkok 10700.​
-        </p> 
-        <p>TEL : 02 424 9888​ </p>
-        <p>FAX : 02 424 9883</p>
-        <p>Email : info.rama8@Amarinhg.com</p> 
+          Hotel Souvenir Shop: The one-stop location for gifts for friends and
+          family, and other sundry items. Located in the hotel lobby.
+        </p>
+        <p>
+          Resort Style Garden: Beautiful Foliage and manicured lawns – the
+          perfect spot for picnics
+        </p>
+        <p>
+          Petra Garden Ballroom: The magnificent Petra Garden Ballroom, a truly
+          magical location for a wedding
+        </p>
+        <p>High Speed Wifi: Complimentary throughout the resort</p>
+        <p>
+          24-Hour Concierge Service: We don’t sleep so you can! From restaurant
+          reservations, tickets to VIP events, arranging tours to recommending
+          theatre and nightlife hotspots, we can assist you
+        </p>
+      </div>
+      <div className="footer has-text-centered">
+        <figure>
+          <Image src={MB2} />
+        </figure>
       </div>
     </footer>
   );
